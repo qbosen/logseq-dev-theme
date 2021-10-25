b66f3d8ef32c1ee5d157c62fedc4e4efba821600
qiubaisen <abosen@qq.com>
2021-10-25 18:10:51 +0800

    add custom css

--- local.sh
+++ local.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-npm install && npm run build
-cp main.css highlights.css sidebar.css zettelkasten.css ~/logseq/data/logseq
+# npm install && npm run build
+# cp main.css highlights.css sidebar.css zettelkasten.css advanced_tags.css ~/logseq/data/logseq
+
+npm install && npm run build:cust
+cp custom.css ~/logseq/data/logseq
