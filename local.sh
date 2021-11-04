61c41866859ff224a735ce34ca0abf13c2d499c8
qiubaisen <abosen@qq.com>
2021-11-04 18:10:40 +0800

    auto commit

--- local.sh
+++ local.sh
@@ -4,4 +4,4 @@
 # cp main.css highlights.css sidebar.css zettelkasten.css advanced_tags.css ~/logseq/data/logseq
 
 npm install && npm run build:cust
-cp custom.css ~/logseq/data/logseq
+cp custom.css custom-without-sidebar.css ~/logseq/data/plugins/my-theme
