7bde51d9db8f1ca057c46d72b9722c108279d2da
qiubaisen <abosen@qq.com>
2022-07-08 16:37:27 +0800

    update code mirror css

--- local.sh
+++ local.sh
@@ -4,5 +4,5 @@
 # cp main.css highlights.css sidebar.css zettelkasten.css advanced_tags.css ~/logseq/data/logseq
 set -x
 npm install && npm run build:cust
-cp custom.css ~/logseq/data/logseq/
+# cp custom.css ~/logseq/data/logseq/
 cp custom.css custom-single-sidebar.css ~/logseq/data/plugins/my-theme/
